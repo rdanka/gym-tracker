eefbe09a921698f3b7637e7dc114acd6a7edcd77
DESKTOP-AFFH5NC\Robi <lobika2001@gmail.com>
2025-09-01 15:53:06 +0200

    made it a pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from 'vite';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+     VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "My Gym Tracker",
+        short_name: "GymApp",
+        description: "Track your workouts and progress",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "logo.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
